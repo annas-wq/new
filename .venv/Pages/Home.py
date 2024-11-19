4d9f3f412e6e2c794345e2186c4b6774f8011293
Darkhan Annas <annasdarkhan@gmail.com>
2024-11-19 10:43:35 +0500

    commit 4

--- .venv/Pages/Home.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-class Home:
-    def __int__(self):
-        pass
-    def app(self):
-        st.write('Home')
--- .venv/Pages/Project1.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-class Project1:
-    def __int__(self):
-        pass
-    def app(self):
-        st.write('Project1')
