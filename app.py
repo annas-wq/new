4f3256d6addf94703f87b58ea1360d69a7ffb2bb
Darkhan Annas <annasdarkhan@gmail.com>
2024-11-24 14:17:22 +0500

    19112024

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import os.path
+
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import numpy as np
+from streamlit_navigation_bar.example import logo_path
+
+image = Image.open('img/genetic-algorithm-svgrepo-com.png')
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 import streamlit as st
 from Pages import Home, Project1, Project2, Project3
 from streamlit_navigation_bar import st_navbar
-st.set_page_config(initial_sidebar_state="collapsed")
 pages = ["Home", "Project1", "Project2", "Project3"]
 
+logo_path = os.path.join(os.path.dirname(__file__), "img", "genetic-algorithm-svgrepo-com.svg")
+pages = [" ", 'Home', 'Project1', 'Project2', 'Project3']
+
+page = st_navbar(pages, styles=styles, logo_path=logo_path, options=options)
 styles = {
     "nav": {
-        "background-color": "rgb(123, 209, 146)",
+        "background-color": "rgb(123, 209, 146)"
     },
     "div": {
         "max-width": "32rem",
@@ -22,6 +35,14 @@ styles = {
     },
     "hover": {
         "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+    "img": {
+        "position": "absolute",
+        "left": "-20px",
+        "font-size": "15px",
+        "top": "4px",
+        "width": "100px",
+        "height": "40px",
     }
 }
 page = st_navbar(pages, styles=styles)
@@ -36,3 +57,6 @@ elif page == "Project3":
     Project3.Project3().app()
 else:
     Home.Home().app()
+
+
+
